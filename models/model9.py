62f61416c93c319192f70f1effbb0ae80d6af8c1
SyedMa3 <sammateen10@outlook.com>
2023-04-01 18:05:10 +0530

    fixed dimension bug in fc Q K V

--- models/model9.py
+++ models/model9.py
@@ -65,7 +65,7 @@ class UltimusBlock(nn.Module):
         XK = self.k(x)
         XK = XK.view(XK.size(0), 1, -1)
         XQ = self.q(x)
-        XQ = XQ.view(XQ.size(0), -1, 1)
+        XQ = XQ.view(XQ.size(0), 1, -1)
         XV = self.v(x)
         XV = XV.view(XV.size(0), 1, -1)
 
